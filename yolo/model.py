e3fe766e52441764cf0c4b70ccebf51fa8a5750e
tungps <tungqg2@gmail.com>
2023-07-23 23:16:37 +0700

    darknet backbone

--- yolo/model.py
+++ yolo/model.py
@@ -35,7 +35,48 @@ def DarknetConv(x, filters, size, strides=1, batch_norm=True):
     x = Conv2D(filters=filters, kernel_size=size, strides=strides,
                padding=padding, use_bias=not batch_norm, kernel_regularizer=l2(0.0005))(x)
     if batch_norm:
-        x = Batch
+        x = BatchNormalization()(x)
+        x = LeakyReLU(alpha=0.1)(x)
+    return x
+
+def DarknetResidual(x, filters):
+    prev = x
+    x = DarknetConv(x, filters // 2, 1)
+    x = DarknetConv(x, filters, 3)
+    x = Add()([prev, x])
+    x
+
+def DarknetBlock(x, filters, blocks):
+    x = DarknetConv(x, filters, 3, strides=2)
+    for _ in range(blocks):
+        x = DarknetResidual(x, filters)
+    return x
+
+def Darknet(name=None):
+    x = inputs = Input([None, None, 3])
+    x = DarknetConv(x, 32, 3)
+    x = DarknetBlock(x, 64, 1)
+    x = DarknetBlock(x, 128, 2)
+    x = x_36 = DarknetBlock(x, 256, 8)
+    x = x_61 = DarknetBlock(x, 512,8)
+    x = DarknetBlock(x, 1024, 4)
+    return tf.keras.Model(inputs, (x_36, x_61, x), name=name)
+
+def YoloConv(filters, name=None):
+    def yolo_conv(x_in):
+        if isinstance(x_in, tuple):
+            inputs = Input(x_in[0].shape[1:]), Input(x_in[1].shape[1:])
+            x, x_skip = inputs
+
+            x = DarknetConv(x, filter, 1)
+            x = DarknetConv(x, filter*2, 3)
+            x = DarknetConv(x, filters, 1)
+            x = DarknetConv(x, filters * 2, 3)
+            x = DarknetConv(x, filters, 1)
+
+            return Model(inputs, x, name=name)(x_in)
+        return yolo_conv
+
     
 
 
